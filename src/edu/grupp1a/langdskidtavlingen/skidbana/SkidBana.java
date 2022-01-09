d868dbc6a30414cc1a6391909fb7efffcf5112b3
Jens Radtke <jens.radtke@yaelev.se>
2022-01-09 21:36:15 +0100

    Fixat utskriften av listor.

--- src/edu/grupp1a/langdskidtavlingen/skidbana/SkidBana.java
+++ src/edu/grupp1a/langdskidtavlingen/skidbana/SkidBana.java
@@ -122,19 +122,6 @@ public class SkidBana {
 		
 	}
 	
-//	public void visaStartLista() {
-//		
-//		double intervallTid = (startandeAkare.get(1).getStartTid() - startandeAkare.get(0).getStartTid())/1000d;		
-//		
-//		System.out.println("\nStartlista:");
-//		
-//		for(int i=0; i<startandeAkare.size();i++) {
-//			
-//			System.out.println("+" + intervallTid*i + " s\t | " + startandeAkare.get(i));			
-//		}
-//
-//	}
-	
 	public void visaStartLista() {
 		
 		double intervallTid = (startandeAkare.get(1).getStartTid() - startandeAkare.get(0).getStartTid())/1000d;		
